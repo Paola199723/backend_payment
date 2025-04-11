d5f527c404b4168bac4175baa9ad154aae4e3ce9
Paola Casadiegos <acsapaola@gmail.com>
2025-04-10 20:10:21 -0500

    se agregan librerias y se conecta a la base de datos local

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',  // Usuario de PostgreSQL
+      password: 'Paola123',  // Contraseña de PostgreSQL
+      database: 'database_venta',  // Nombre de la base de datos (se creará automáticamente si no existe)
+      autoLoadEntities: true, // Carga las entidades automáticamente
+      synchronize: true, // Crea las tablas automáticamente (NO usar en producción)
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true, // Así no necesitas importar en cada módulo
+    }),
+  ],
 })
 export class AppModule {}
+
