4912186d222fda043069fe562ff1c97af7e2e152
Paola Casadiegos <acsapaola@gmail.com>
2025-04-14 04:51:20 -0500

    se agrega variable de entorno

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:5173', // Permite el frontend de Vite
+    origin: process.env.URL,
   });
   await app.listen(process.env.PORT || 3000);
 }
