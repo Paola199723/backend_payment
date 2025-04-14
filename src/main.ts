5046b7f288a6db5ca424431b3a6d65e7e5613ecf
Paola Casadiegos <acsapaola@gmail.com>
2025-04-14 00:56:26 -0500

    se ajusta el puerto

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:5173', // Permite el frontend de Vite
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
